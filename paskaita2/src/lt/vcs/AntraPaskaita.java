7c938f9211dffa359394c726e025598aec7f8155
Aleksandras <aleksandras@Aleksandrass-MacBook-Pro.local>
2016-11-15 21:20:45 +0200

    Antra paskaita

---
+++ paskaita2/src/lt/vcs/AntraPaskaita.java
@@ -0,0 +1,43 @@
+package lt.vcs;
+
+/**
+ *
+ * @author aleksandras
+ */
+public class AntraPaskaita {
+
+    static int betkasp;
+    static Float kas;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Helloooooo");
+        
+        //primityvus kintamieji                   Objektiniai kintamieji
+        
+        byte baitas = 77;                       Byte baitas2 = new Byte(baitas);
+        short trumpas = 7777;                   Short trumpas2 = new Short(trumpas);
+        int intas = 777777777;                  Integer intas2 = new Integer(777);
+        long longas = 7777777777777777777L;     Long longas2 = new Long(777);
+        
+        float floatas = 777.7635f;              Float floatas2 = new Float(77.3432);
+        double doubleas = 7777.787878787877;    Double doubleas2 = new Double(doubleas);
+                                                
+        char charas = 'w';                      Character charas2 = new Character(charas);
+        boolean kazkas = true;                  Boolean kazkas2 = new Boolean(false);
+        
+        
+        
+        
+        
+        System.out.println(kas);
+        
+        
+        
+               
+    }
+    
+}
