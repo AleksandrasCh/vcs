97cd9cf1d67fb4321f92201aa44f0707a6c994ea
Aleksandras <aleksandras.ch@gmail.com>
2016-11-16 10:19:38 +0200

    2 paskaitos ND

---
+++ paskaita2nd/src/lt/vcs/Paskaita2Nd.java
@@ -0,0 +1,15 @@
+package lt.vcs;
+
+/**
+ *
+ * @author aleksandras
+ */
+public class Paskaita2Nd {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Padariau namu darbus :)");
+
+    }
+    
+}
