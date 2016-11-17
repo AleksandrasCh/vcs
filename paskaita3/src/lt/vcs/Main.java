fdd530dc905046503198e905dc4c3e1da67fed94
Aleksandras <aleksandras.ch@gmail.com>
2016-11-17 10:24:11 +0200

    3 paskaita

--- paskaita3/src/lt/vcs/Main.java
+++ paskaita3/src/lt/vcs/Main.java
@@ -1,16 +1,67 @@
 package lt.vcs;
 
+import java.util.Scanner;
+
 /**
  *
  * @author aleksandras
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+//        int skaicius = 7;
+//        int antras = 1;
+//        
+//        System.out.println(antras);
+//        antras = antras + skaicius;
+//        System.out.println(antras);
+//        
+//        int trecias = antras % 2;
+//        System.out.println(trecias);
+        
+        //siaip("kdsjfksdjksdjf"); // uztenka vieno stringo nusiusti i siaip metoda
+        
+       
+       String skaicius = Pirmas(5);
+        
+    
+        System.out.println("Skaicius tekste " + skaicius);
+        
+        String masyvas = "masyvas";
+        char [] mass = masyvas.toCharArray();
+        
+        Scanner scan = new Scanner(System.in);
+//        System.out.println("Iveskite skaiciu");
+//        int sk1 = scan.nextInt();
+//        System.out.println("Skaicius " + sk1);
+        
+       String tekstas = "Iveskite skaiciu";
+       int sk4 = trecias(tekstas);
+       System.out.println("Ivestas skaicius " + sk4);
+           
     }
+    
+    
+    public static void siaip(String param1, String... param2){ // i parametra su "..." nebutina siusti info
+            
+    }
+    
+    public static String Pirmas (int skaicius2){
+        
+        int skaicius = 5;
+        Antras(skaicius);
+        
+        String tekstas = Integer.toString(skaicius2);       // Galima tiesiog apsirasyti kaip String str = "" + skaicius2
+       
+        
+        return tekstas;
+    }
+    
+    public static int Antras(int skaicius){
+        
+        return skaicius;
+    }
+    
     
 }
