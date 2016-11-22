86f6daae270ed605384c647b4832430c01e3b654
Aleksandras <aleksandras.ch@gmail.com>
2016-11-22 17:46:40 +0200

    update

---
+++ dicePoker/src/lt/vcs/domain/CheckWinner.java
@@ -0,0 +1,33 @@
+package lt.vcs.domain;
+
+/**
+ *
+ * @author aleksandras
+ */
+public class CheckWinner {
+    
+    public static int checkWinner(String text){
+        int taskai = 0;
+        
+        switch (text){
+            
+            case "Five of a kind ":
+                break;
+            case "Four of a kind ":
+                break;
+            case "Full House ":
+                break;
+            case "Three of a Kind ":
+                break;
+            case "Two Pairs ":
+                break;
+            case "One Pair ":
+                break;
+                
+        }
+                
+        
+        return taskai;
+    }
+    
+}
