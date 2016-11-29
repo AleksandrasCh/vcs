85986aec3a34dda2dfd220029b424a64966f11fb
Aleksandras <aleksandras.ch@gmail.com>
2016-11-29 14:34:24 +0200

    update

--- paskaita6/src/lt/vcs/Main.java
+++ paskaita6/src/lt/vcs/Main.java
@@ -1,5 +1,6 @@
 package lt.vcs;
 
+import java.sql.SQLException;
 import static lt.vcs.VcsUtils.*;
 
 /**
@@ -11,17 +12,54 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static boolean zaisti = false;
+    
+    public static void main(String[] args) throws SQLException{
         
+
         println("Kauliuku pokeris");
         String p1name = inputWord("Zaidejas 1, iveskite savo varda");
+        
+        Login newLogin = new Login (p1name);
+        boolean login = newLogin.checkUser();
+        
+        if (login == false){
+            int ats = inputInt("Naudotojo vardas " + p1name + " nerastas. Ar noresite uzsiregistruoti? (1 = taip; 2 = ne)");
+                if(ats == 1){
+                    Registration newReg = new Registration (p1name);
+                    newReg.newUser();
+                } else {
+                    zaisti = false;
+                }
+        } else {
+            zaisti = true;
+        }
+        
         String p2name = inputWord("Zaidejas 2, iveskite savo varda");
+        
+        Login newLogin2 = new Login (p2name);
+        login = newLogin2.checkUser();
+        
+        if (login == false){
+            int ats = inputInt("Naudotojo vardas " + p2name + " nerastas. Ar noresite uzsiregistruoti? (1 = taip; 2 = ne)");
+                if(ats == 1){
+                    Registration newReg = new Registration (p2name);
+                    newReg.newUser();
+                } else {
+                    zaisti = false;
+                }
+        } else {
+            zaisti = true;
+        }
         
         Player p1 = new Player(p1name);
         Player p2 = new Player(p2name);
-        boolean zaisti = true;
-
-        while (zaisti) {
+        
+        if (zaisti == false){
+            println("Zaidimas negalimas, nes nera prisijunge 2 zaidejai");
+        }
+        
+        while (zaisti == true) {
             Game newGame = new Game(p1, p2);
             newGame.start();
             String pasirinkimas = inputWord("Ar norite zaisti toliau? (y - taip; n - ne)");
